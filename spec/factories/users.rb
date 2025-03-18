6f814b3ab6899eaf8fd48331f18580c24894a53d
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-18 16:02:57 +0900

    ユーザーモデルテストコード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-
-
-
-
-    
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    nickname              { Faker::Name.name }
+    last_name             { Faker::Name.last_name }
+    first_name            { Faker::Name.first_name }
+    last_name_kana        { "ヤマダ" }
+    first_name_kana       { "タロウ" }
+    birth_date            { "2000-01-01" }
   end
 end
