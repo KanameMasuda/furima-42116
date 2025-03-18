e5c92c2c1bd2e287690b9f63673ab1461fb7330e
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-18 22:28:07 +0900

    bundle exec rubocop -a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :last_name, presence: true, format: { 
-           with: /\A[ぁ-んァ-ン一-龥々]+\z/, 
-           message: "はひらがな・カタカナ・漢字のみ使用できます" 
-         }
-         validates :first_name, presence: true, format: { 
-           with: /\A[ぁ-んァ-ン一-龥々]+\z/, 
-           message: "はひらがな・カタカナ・漢字のみ使用できます" 
-         }
-         validates :last_name_kana, presence: true, format: { 
-           with: /\A[ァ-ヶー]+\z/, 
-           message: "はカタカナのみ使用できます" 
-         }
-         validates :first_name_kana, presence: true, format: { 
-           with: /\A[ァ-ヶー]+\z/, 
-           message: "はカタカナのみ使用できます" 
-         }
-         validates :birth_date, presence: true
-         validates :password, format: { 
-           with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, 
-           message: "は英数字を両方含める必要があります" 
-         }, if: -> { password.present? }
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: {
+    with: /\A[ぁ-んァ-ン一-龥々]+\z/,
+    message: 'はひらがな・カタカナ・漢字のみ使用できます'
+  }
+  validates :first_name, presence: true, format: {
+    with: /\A[ぁ-んァ-ン一-龥々]+\z/,
+    message: 'はひらがな・カタカナ・漢字のみ使用できます'
+  }
+  validates :last_name_kana, presence: true, format: {
+    with: /\A[ァ-ヶー]+\z/,
+    message: 'はカタカナのみ使用できます'
+  }
+  validates :first_name_kana, presence: true, format: {
+    with: /\A[ァ-ヶー]+\z/,
+    message: 'はカタカナのみ使用できます'
+  }
+  validates :birth_date, presence: true
+  validates :password, format: {
+    with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+    message: 'は英数字を両方含める必要があります'
+  }, if: -> { password.present? }
 end
