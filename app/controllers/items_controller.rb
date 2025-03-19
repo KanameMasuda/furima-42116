0e0d4ad2ed220efc6cc85ed570ab3bb86cbeb299
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-20 08:28:08 +0900

    itemコントローラーの作成ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_days_id, :image).merge(user_id: current_user.id)
+  end
 end
