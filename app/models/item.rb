4b41e4ca7aa9022e7aa16a9c12e8b91ebea07882
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-23 15:16:30 +0900

    rubocop修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be a number between 300 and 9,999,999" }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be a number between 300 and 9,999,999' }
   validates :image, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: 'を選択してください' }
   validates :condition_id, presence: true, numericality: { other_than: 1, message: 'を選択してください' }
