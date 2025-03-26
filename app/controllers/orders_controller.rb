c97b2d515bfc5b613ce8a47cd495e6f419fd7cd5
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-26 16:19:45 +0900

    購入機能に必要なモデルやコントローラーを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,31 @@
 class OrdersController < ApplicationController
-  
+
   def index
     @item = Item.find(params[:item_id])
-    @order_form = Order.new
+    @order = Order.new(item_id: @item.id)
   end
 
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :user_id, :item_id, :postal_code, :prefecture_id, 
+      :city, :house_number, :phone_number
+    )
+  end
 end
